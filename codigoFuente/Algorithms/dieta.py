cb24ff18ebcea1abadabc6b8d31e7955dc01b339
Cesar Arley Ojeda Escobar <mas.arley2009@hotmail.com>
2019-12-05 18:26:53 -0600

    project finished

--- codigoFuente/Algorithms/dieta.py
+++ codigoFuente/Algorithms/dieta.py
@@ -11,61 +11,42 @@ class Dieta:
         self.max_hierro = max_hierro
         self.datos = datos
 
-    def f(self, cromosoma):
-        kc, vit, fib, cal, hie = 0, 0, 0, 0, 0
-
-        m_kc, m_vit, m_fib, m_cal, m_hie = False, False, False, False, False
-
-        # f = 0
-        # kcal = 0
-        # for ind, sel, in enumerate(cromosoma):
-        #     if sel:
-        #         f = f + self.datos[ind][2]
-        #         kcal = kcal + self.datos[ind][1]
-        # if kcal < self.max_kcal:
-        #     return f
-        # else:
-        #     return 0
-        
-        # Producto,Kcal,Vitamina A,Fibra,Calcio,Hierro
-        #   0       1       2       3       4      5
-
-        for index, sel in enumerate(cromosoma):
-            if sel:
-                kc += self.datos[index][1]
-                vit += self.datos[index][2]
-                fib += self.datos[index][3]
-                cal += self.datos[index][4]
-                hie += self.datos[index][5]
-
-        if self.max_kcal != -1:
-            m_kc = kc < self.max_kcal
-        else:
-            m_kc = True
-        if self.max_vit != -1:
-            m_vit = vit < self.max_vit
-        else:
-            m_vit = True
-        if self.max_fibra != -1:
-            m_fib = fib < self.max_fibra
-        else:
-            m_fib = True
-        if self.max_calcio != -1:
-            m_cal = cal < self.max_calcio
-        else:
-            m_cal = True
-        if self.max_hierro != -1:
-            m_hie = hie < self.max_hierro
-        else:
-            m_hie = True
-
-        if m_kc and m_vit and m_fib and m_cal and m_hie:
-            return kc + vit + fib + cal + hie
-        # if m_kc:
-        #     return vit + fib + cal + hie
-            # return vit
-        else:
-            return 0
+def f(self, cromosoma):
+    kc, vit, fib, cal, hie = 0, 0, 0, 0, 0
+    m_kc, m_vit, m_fib, m_cal, m_hie = False, False, False, False, False
+    # Producto,Kcal,Vitamina A,Fibra,Calcio,Hierro
+    #   0       1       2       3       4      5
+    for index, sel in enumerate(cromosoma):
+        if sel:
+            kc += self.datos[index][1]
+            vit += self.datos[index][2]
+            fib += self.datos[index][3]
+            cal += self.datos[index][4]
+            hie += self.datos[index][5]
+    if self.max_kcal != -1:
+        m_kc = kc < self.max_kcal
+    else:
+        m_kc = True
+    if self.max_vit != -1:
+        m_vit = vit < self.max_vit
+    else:
+        m_vit = True
+    if self.max_fibra != -1:
+        m_fib = fib < self.max_fibra
+    else:
+        m_fib = True
+    if self.max_calcio != -1:
+        m_cal = cal < self.max_calcio
+    else:
+        m_cal = True
+    if self.max_hierro != -1:
+        m_hie = hie < self.max_hierro
+    else:
+        m_hie = True
+    if m_kc and m_vit and m_fib and m_cal and m_hie:
+        return kc + vit + fib + cal + hie
+    else:
+        return 0
 
 if __name__ == "__main__":
     alimentos = []
